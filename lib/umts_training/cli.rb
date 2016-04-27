7077f4e7f5f3d9ade27064f131e039342f2b0dd0
Matt Moretti <werebus@gmail.com>
2016-04-27 11:54:59 -0400

    Support for external messages file

--- lib/umts_training/cli.rb
+++ lib/umts_training/cli.rb
@@ -1,5 +1,14 @@
 module UMTSTraining
   class CLI < ::HighLine
+    def initialize(messages_file)
+      super($stdin, $stdout, 80)
+      @message_catalog = YAML.load(File.open(messages_file))
+    end
+
+    def message(name)
+      say @message_catalog.fetch(name)
+    end
+
     def pw_ask(prompt)
       ask(prompt) { |q| q.echo = '*' }
     end
