ef14a2e1f0f083aeb3bc74fef906ac7a956e2d5a
Matt Moretti <werebus@gmail.com>
2016-04-27 11:56:11 -0400

    Store collaborators not in bootstrap

--- lib/umts_training.rb
+++ lib/umts_training.rb
@@ -5,3 +5,11 @@ require 'umts_training/cli'
 require 'umts_training/client'
 require 'umts_training/local_repo'
 require 'umts_training/milestone'
+
+module UMTSTraining
+  def self.collaborators
+    # Sadly getting members of a team via the API requires a level of
+    # permission on the organization that our trainees won't have.
+    %w(werebus sherson dfaulken)
+  end
+end
