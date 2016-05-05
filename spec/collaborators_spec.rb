fd925ce4f202e33b25cf8b2ddfd99950054447dc
Matt Moretti <werebus@gmail.com>
2016-05-05 10:22:36 -0400

    Tiny collaborators spec
    Plus a reduction in magic numbers in the milestone spec

---
+++ spec/collaborators_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe UMTSTraining do
+  describe '.collaborators' do
+    it 'is an array of strings valid as GitHub usernames' do
+      expect(UMTSTraining.collaborators).to all(match(/^[a-zA-Z0-9-]+$/))
+    end
+  end
+end
--- spec/milestone_spec.rb
+++ spec/milestone_spec.rb
@@ -37,31 +37,37 @@ RSpec.describe UMTSTraining::Milestone do
         .with('judy/harfbang', 'Training exit interview')
         .and_return('ANYTHING').once
 
-      first_call = milestone.create!
-      second_call = milestone.create!
-      expect(first_call).to eq(second_call)
+      first_milestone = milestone.create!
+      second_milestone = milestone.create!
+      expect(first_milestone).to be(second_milestone)
     end
   end
 
   describe '#create_issues!' do
     before :each do
+      @ms_num = 1
+
+      yaml_file = milestone.instance_variable_get(:@yaml)
+      @document_count = YAML.load_stream(yaml_file).count
+      yaml_file.rewind
+
       allow(client).to receive(:create_milestone)
-        .and_return(OpenStruct.new(number: 1))
+        .and_return(OpenStruct.new(number: @ms_num))
       milestone.create!
     end
     it 'creates one issue for each YAML doc' do
-      expect(client).to receive(:create_issue).exactly(3).times
+      expect(client).to receive(:create_issue).exactly(@document_count).times
       milestone.create_issues!
     end
     it 'uses the repository name' do
       expect(client).to receive(:create_issue)
-        .with('judy/harfbang', any_args).exactly(3).times
+        .with('judy/harfbang', any_args).exactly(@document_count).times
       milestone.create_issues!
     end
     it 'uses the milestone number' do
       expect(client).to receive(:create_issue)
-        .with(anything, anything, anything, hash_including(milestone: 1))
-        .exactly(3).times
+        .with(anything, anything, anything, hash_including(milestone: @ms_num))
+        .exactly(@document_count).times
       milestone.create_issues!
     end
     it 'uses the title for an issue' do
