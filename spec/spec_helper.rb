b08c2bee804fac6d9fe3bdd81bf0043df4b620ad
Matt Moretti <werebus@gmail.com>
2018-11-21 09:01:06 -0500

    Made some rspec config adjustments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,16 @@ $LOAD_PATH.unshift File.expand_path(File.join(this_dir, '..', 'lib'))
 require 'umts_training'
 
 RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.disable_monkey_patching!
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -16,8 +26,4 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-  config.disable_monkey_patching!
-
-  config.default_formatter = 'doc' if config.files_to_run.one?
 end
