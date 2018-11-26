f9d38b09c78813a23dddddbef3bc4ca93e9f07a6
Matt Moretti <werebus@gmail.com>
2018-11-26 13:29:25 -0500

    LocalRepo docs

--- lib/umts_training/local_repo.rb
+++ lib/umts_training/local_repo.rb
@@ -3,9 +3,17 @@
 require 'git'
 
 module UMTSTraining
+  ##
+  # Represents the git repository on disk. It uses the "origin" remote of
+  # that repository to figure out the username and repository name on GitHub.
   class LocalRepo
-    attr_reader :user, :name
+    # The remote username
+    attr_reader :user
+    # The remote repository name
+    attr_reader :name
 
+    ##
+    # `directory` is the path to a directory that contains a git repository.
     def initialize(directory)
       # git@github.com:user/repo.git  -or-
       # https://github.com/user/repo.git
@@ -17,6 +25,9 @@ module UMTSTraining
       @name = match[:repo]
     end
 
+    ##
+    # "<user>/<name". This is the format GitHub uses to refer to repositories
+    # in their API.
     def github_name
       @user + '/' + @name
     end
